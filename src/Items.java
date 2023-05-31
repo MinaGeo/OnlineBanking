af688a6e52b2c924f43278e6a8f0b57cd92eecb6
Mohamed Khafagy <mohamed.tarek.khafagy@gmail.com>
2023-05-31 04:45:17 +0300

    items modified

--- src/Items.java
+++ src/Items.java
@@ -1,2 +1,27 @@
 public class Items {
+    String name;
+    String id;
+    double price;
+    int count;
+
+
+    public void buy(){
+        count--;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
